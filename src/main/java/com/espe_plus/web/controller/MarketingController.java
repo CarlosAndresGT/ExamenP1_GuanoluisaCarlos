ea3b9acb9bc601cd7f6cdf32a9b347c16633aacc
Andrescgtw <caguanoluisa@espe.edu.ec>
2026-05-28 20:55:32 -0500

    exponer endpoint get de marketing

---
+++ src/main/java/com/espe_plus/web/controller/MarketingController.java
@@ -0,0 +1,23 @@
+package com.espe_plus.web.controller;
+
+import com.espe_plus.domain.service.EduSmartAIService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarketingController {
+
+    private final EduSmartAIService aiService;
+
+    public MarketingController(EduSmartAIService aiService) {
+        this.aiService = aiService;
+    }
+
+    @GetMapping("/api/edusmart/publicidad")
+    public String generarPublicidad(
+            @RequestParam("tema") String tema,
+            @RequestParam("audiencia") String audiencia) {
+        return aiService.generateGreeting(tema,audiencia);
+    }
+}
