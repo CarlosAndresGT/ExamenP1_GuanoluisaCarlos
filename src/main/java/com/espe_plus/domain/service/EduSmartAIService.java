95ac1bba4e08e98623a63d1518d05a8f42bd0aa9
Andrescgtw <caguanoluisa@espe.edu.ec>
2026-05-28 20:39:35 -0500

    integrar langchain4j para publicidad

---
+++ src/main/java/com/espe_plus/domain/service/EduSmartAIService.java
@@ -0,0 +1,15 @@
+package com.espe_plus.domain.service;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.V;
+
+@AiService
+public interface EduSmartAIService {
+
+    @UserMessage("""
+            Redacta una frase publicitaria de máximo 100 caracteres para vender un curso de
+            {{tema}} dirigido a {{audiencia}}.
+            """)
+    String generateGreeting(@V("platform") String platform);
+}
